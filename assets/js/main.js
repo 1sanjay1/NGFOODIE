d994bbc3844206870b4c6d6f4a49bd1b60c4d8bd
1sanjay1 <sanjayucp1554@gmail.com>
2017-07-20 07:14:03 +0530

    till 3rd class

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
-// // Runs when HTML file is loaded
-// $('#user-email').on('input',function() {
-//     var email = $('#user-email').val();
-//     var message = 'Welcome Back, ' + email;
-//     $('.welcome-message').text(message);
-// });
+var foodieApp = angular.module('foodieApp',[]);
+console.log(foodieApp);
+
+foodieApp.controller('mainController',function($scope) {
+    $scope.restaurants = ['Farzi Cafe','Pizza Hut','Wenger\'s Deli','Sagar Ratna'];
+});
