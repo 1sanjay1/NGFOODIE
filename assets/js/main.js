d694989b9587b75d29f9b40c022c173030f69240
1sanjay1 <sanjayucp1554@gmail.com>
2017-07-23 23:59:32 +0530

    fixed top navigation bar and removing extra padding and margin in the rows

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,20 +2,5 @@ var foodieApp = angular.module('foodieApp',[]);
 console.log(foodieApp);
 
 foodieApp.controller('mainController',function($scope) {
-    
-//    $scope.restaurants = ['Farzi Cafe','Pizza Hut','Wenger\'s Deli','Sagar Ratna'];
-//list of objects which represent foods
-    
-$scope.restaurants = [{
-	name: 'Farzi Cafe',
-	address: '38/39, Level 1, Block E , Inner Circle, Connaught Place',
-	location: 'Connaught Place',
-	category: 'Casual Dining, Bar',
-	vote: '4.2',
-	cuisines: 'Modern Indian',
-	cost: '2200',
-	hours: '12 Noon to 1 AM (Mon-Sun)',
-	image: 'https://b.zmtcdn.com/data/pictures/chains/2/308022/dabd30bd0b000ea859ada9a08a0132fc.jpg'
-}];
-    
+    $scope.restaurants = ['Farzi Cafe','Pizza Hut','Wenger\'s Deli','Sagar Ratna'];
 });
