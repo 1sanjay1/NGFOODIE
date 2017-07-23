6b2d1b17f07110463027ec2328f848298e48f8e8
1sanjay1 <sanjayucp1554@gmail.com>
2017-07-24 03:24:33 +0530

    finish search bar and middle logo image and search bar is connected with searching result using filter in AngularJS

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,5 +2,63 @@ var foodieApp = angular.module('foodieApp',[]);
 console.log(foodieApp);
 
 foodieApp.controller('mainController',function($scope) {
-    $scope.restaurants = ['Farzi Cafe','Pizza Hut','Wenger\'s Deli','Sagar Ratna'];
+
+  // first object
+  $scope.restaurants = [{
+    name: 'Farzi Cafe',
+    address: '38/39, Level 1, Block E , Inner Circle, Connaught Place',
+    location: 'Connaught Place',
+    category: 'Casual Dining, Bar',
+    vote: '4.2',
+    cuisines: 'Modern Indian',
+    cost: '2200',
+    hours: '12 Noon to 1 AM (Mon-Sun)',
+    image: 'https://b.zmtcdn.com/data/pictures/chains/2/308022/dabd30bd0b000ea859ada9a08a0132fc.jpg'
+  },
+  {
+    name: 'Farzi1 Cafe',
+    address: '38/39, Level 1, Block E , Inner Circle, Connaught Place',
+    location: 'Connaught Place',
+    category: 'Casual Dining, Bar',
+    vote: '4.2',
+    cuisines: 'Modern Indian',
+    cost: '2200',
+    hours: '12 Noon to 1 AM (Mon-Sun)',
+    image: 'https://b.zmtcdn.com/data/pictures/chains/2/308022/dabd30bd0b000ea859ada9a08a0132fc.jpg'
+  },
+  {
+    name: 'Farzi2 Cafe',
+    address: '38/39, Level 1, Block E , Inner Circle, Connaught Place',
+    location: 'Connaught Place',
+    category: 'Casual Dining, Bar',
+    vote: '4.2',
+    cuisines: 'Modern Indian',
+    cost: '2200',
+    hours: '12 Noon to 1 AM (Mon-Sun)',
+    image: 'https://b.zmtcdn.com/data/pictures/chains/2/308022/dabd30bd0b000ea859ada9a08a0132fc.jpg'
+  },
+  {
+    name: 'Farzi3 Cafe',
+    address: '38/39, Level 1, Block E , Inner Circle, Connaught Place',
+    location: 'Connaught Place',
+    category: 'Casual Dining, Bar',
+    vote: '4.2',
+    cuisines: 'Modern Indian',
+    cost: '2200',
+    hours: '12 Noon to 1 AM (Mon-Sun)',
+    image: 'https://b.zmtcdn.com/data/pictures/chains/2/308022/dabd30bd0b000ea859ada9a08a0132fc.jpg'
+  },
+  {
+    name: 'Farzi4 Cafe',
+    address: '38/39, Level 1, Block E , Inner Circle, Connaught Place',
+    location: 'Connaught Place',
+    category: 'Casual Dining, Bar',
+    vote: '4.2',
+    cuisines: 'Modern Indian',
+    cost: '2200',
+    hours: '12 Noon to 1 AM (Mon-Sun)',
+    image: 'https://b.zmtcdn.com/data/pictures/chains/2/308022/dabd30bd0b000ea859ada9a08a0132fc.jpg'
+  }];
+
+
 });
