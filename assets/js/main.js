e786e64e2a73a39e4110ca46b2ba97ed27ab2585
1sanjay1 <sanjayucp1554@gmail.com>
2017-07-24 12:43:24 +0530

    implement routing in AngularJS

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,27 @@
-var foodieApp = angular.module('foodieApp',[]);
-console.log(foodieApp);
+var foodieApp = angular.module('foodieApp',['ngRoute']);
+
+// Adding route to our website
+foodieApp.config(function ($routeProvider) {
+
+	$routeProvider
+	.when('/',{
+		templateUrl: 'assets/pages/login.html',
+		controller: 'loginController'
+	})
+	.when('/home',{
+		templateUrl: 'assets/pages/main.html',
+		controller: 'mainController'
+	})
+
+})
+
+
+// login Controller
+foodieApp.controller('loginController',function($scope) {
+
+
+
+})
 
 foodieApp.controller('mainController',function($scope) {
 
@@ -16,10 +38,10 @@ foodieApp.controller('mainController',function($scope) {
     image: 'https://b.zmtcdn.com/data/pictures/chains/2/308022/dabd30bd0b000ea859ada9a08a0132fc.jpg'
   },
   {
-    name: 'Farzi1 Cafe',
+    name: 'Alkauser',
     address: '38/39, Level 1, Block E , Inner Circle, Connaught Place',
-    location: 'Connaught Place',
-    category: 'Casual Dining, Bar',
+    location: 'Chanakyapuri',
+    category: 'TAKEAWAY DELIVERY',
     vote: '4.2',
     cuisines: 'Modern Indian',
     cost: '2200',
@@ -27,7 +49,7 @@ foodieApp.controller('mainController',function($scope) {
     image: 'https://b.zmtcdn.com/data/pictures/chains/2/308022/dabd30bd0b000ea859ada9a08a0132fc.jpg'
   },
   {
-    name: 'Farzi2 Cafe',
+    name: 'Rajwada',
     address: '38/39, Level 1, Block E , Inner Circle, Connaught Place',
     location: 'Connaught Place',
     category: 'Casual Dining, Bar',
@@ -61,4 +83,4 @@ foodieApp.controller('mainController',function($scope) {
   }];
 
 
-});
+})
